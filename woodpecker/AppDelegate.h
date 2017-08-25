0f17cd036de91a425d67e92f9fd7cb2e1d007f07
yongche <xiaojun@yongche.com>
2017-08-25 09:19:49 +0800

    Initial Commit

---
+++ woodpecker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  woodpecker
+//
+//  Created by yongche on 17/8/25.
+//  Copyright © 2017年 goldsmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ woodpecker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  woodpecker
+//
+//  Created by yongche on 17/8/25.
+//  Copyright © 2017年 goldsmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
