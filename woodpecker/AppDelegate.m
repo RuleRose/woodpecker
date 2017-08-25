31d77e8fc516cd1401d0cda1c8344936838847cb
yongche <xiaojun@yongche.com>
2017-08-25 21:50:59 +0800

    assets

--- woodpecker/AppDelegate.m
+++ woodpecker/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
+
 
 @interface AppDelegate ()
 
@@ -17,6 +19,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    
+    ViewController *viewController = [[ViewController alloc] init];
+    self.window.rootViewController = viewController;
+    
+    self.window.backgroundColor = [UIColor purpleColor];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
