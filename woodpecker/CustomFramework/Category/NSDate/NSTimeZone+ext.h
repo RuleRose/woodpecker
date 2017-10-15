aec2989d3c194e8773ae996fcf80a97816dc4d57
yongche <xiaojun@yongche.com>
2017-10-15 14:01:17 +0800

    wheel view pageable

---
+++ woodpecker/CustomFramework/Category/NSDate/NSTimeZone+ext.h
@@ -0,0 +1,13 @@
+//
+//  NSTimeZone+ext.h
+//  woodpecker
+//
+//  Created by yongche on 17/10/15.
+//  Copyright © 2017年 goldsmith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSTimeZone (ext)
++(NSInteger)timeZoneDiffwithUTC;
+@end
