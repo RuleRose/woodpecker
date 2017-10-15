dfe6fc3a0ab021f05858e3d499e12f1e4b8560b1
yongche <xiaojun@yongche.com>
2017-10-15 11:03:25 +0800

    bug fixing

--- woodpecker/CommonManager/WPConnectDeviceManager.m
+++ woodpecker/CommonManager/WPConnectDeviceManager.m
@@ -8,6 +8,7 @@
 
 #import "WPConnectDeviceManager.h"
 #import "MMCDeviceManager.h"
+#import "WPDeviceModel.h"
 
 @interface WPConnectDeviceManager ()
 @property(nonatomic, strong)NSTimer *timer;
@@ -18,15 +19,15 @@ Singleton_Implementation(WPConnectDeviceManager);
 - (instancetype)init {
     self = [super init];
     if (self) {
-        _device = [[WPDeviceModel alloc] init];
     }
     return self;
 }
 
 - (void)startTimer {
+    WPDeviceModel *device = [[WPDeviceModel alloc] init];
     NSDictionary *deviceDic = kDefaultObjectForKey(USER_DEFAULT_DEVICE);
-    [_device loadDataFromkeyValues:deviceDic];
-    if (![NSString leie_isBlankString:_device.mac_addr] && [MMCDeviceManager defaultInstance].deviceConnectionState == STATE_DEVICE_NONE) {
+    [device loadDataFromkeyValues:deviceDic];
+    if (![NSString leie_isBlankString:device.mac_addr] && [MMCDeviceManager defaultInstance].deviceConnectionState == STATE_DEVICE_NONE) {
         if (self.timer)
             [self stopTimer];
         self.timer = [NSTimer timerWithTimeInterval:60*5 target:self selector:@selector(connectDevice) userInfo:nil repeats:YES];
@@ -42,10 +43,11 @@ Singleton_Implementation(WPConnectDeviceManager);
 }
 
 - (void)connectDevice{
+    WPDeviceModel *device = [[WPDeviceModel alloc] init];
     NSDictionary *deviceDic = kDefaultObjectForKey(USER_DEFAULT_DEVICE);
-    [_device loadDataFromkeyValues:deviceDic];
-    if (![NSString leie_isBlankString:_device.mac_addr]) {
-        [[MMCDeviceManager defaultInstance] startScanAndConnectWithMac:_device.mac_addr callback:^(NSInteger sendState) {
+    [device loadDataFromkeyValues:deviceDic];
+    if (![NSString leie_isBlankString:device.mac_addr]) {
+        [[MMCDeviceManager defaultInstance] startScanAndConnectWithMac:device.mac_addr callback:^(NSInteger sendState) {
             
         }];
     }
