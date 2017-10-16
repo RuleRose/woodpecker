bce36930bbf12072498cbcb6f5daada1d8ae102e
RuleRose <15801098006@163.com>
2017-10-17 00:42:25 +0800

    temp data

--- woodpecker/ViewModel/WPMainViewModel.h
+++ woodpecker/ViewModel/WPMainViewModel.h
@@ -13,10 +13,6 @@
 #import "WPTemperatureModel.h"
 
 @interface WPMainViewModel : NSObject
-@property(nonatomic, strong)WPUserModel *user;
-@property(nonatomic, strong)WPProfileModel *profile;
-@property(nonatomic, strong)WPDeviceModel *device;
-
 @property (nonatomic,strong) NSMutableArray *controllerList;
 - (void)updateData;
 - (void)insertTemperature:(WPTemperatureModel *)temp;
--- woodpecker/ViewModel/WPMainViewModel.m
+++ woodpecker/ViewModel/WPMainViewModel.m
@@ -16,6 +16,7 @@
 #import "NSDate+Extension.h"
 #import "WPTemperatureModel.h"
 #import "MMCDeviceManager.h"
+#import "NSDate+ext.h"
 
 @implementation WPMainViewModel
 - (instancetype)init {
@@ -36,12 +37,6 @@
         [myVC.tabBarItem setImage:[[UIImage imageNamed:@"btn-tab-me-u"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
         [myVC.tabBarItem setSelectedImage:[[UIImage imageNamed:@"btn-tab-me-p"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
         [_controllerList addObject:myVC];
-        NSDictionary *userDic = kDefaultObjectForKey(USER_DEFAULT_ACCOUNT_USER);
-        NSDictionary *profileDic = kDefaultObjectForKey(USER_DEFAULT_PROFILE);
-        NSDictionary *deviceDic = kDefaultObjectForKey(USER_DEFAULT_DEVICE);
-        [_user loadDataFromkeyValues:userDic];
-        [_profile loadDataFromkeyValues:profileDic];
-        [_device loadDataFromkeyValues:deviceDic];
     }
     return self;
 }
@@ -50,12 +45,9 @@
     [WPNetInterface getUserinfoWithUserId:kDefaultObjectForKey(USER_DEFAULT_USER_ID) password:kDefaultObjectForKey(USER_DEFAULT_ACCOUNT_TOKEN) success:^(NSDictionary* userDic) {
         if (userDic) {
             kDefaultSetObjectForKey(userDic, USER_DEFAULT_ACCOUNT_USER);
-            _user = [[WPUserModel alloc] init];
-            [_user loadDataFromkeyValues:userDic];
             [self getProfile];
             [self getDevice];
         }else{
-            _user = nil;
             kDefaultRemoveForKey(USER_DEFAULT_ACCOUNT_USER);
         }
     } failure:^(NSError *error) {
@@ -65,19 +57,20 @@
 }
 
 - (void)getProfile{
-    if (![NSString leie_isBlankString:_user.profile_id]) {
-        [WPNetInterface getProfileWithId:_user.profile_id success:^(NSDictionary *profileDic) {
+    WPUserModel *user = [[WPUserModel alloc] init];
+    [user loadDataFromkeyValues:kDefaultObjectForKey(USER_DEFAULT_ACCOUNT_USER)];
+    if (![NSString leie_isBlankString:user.profile_id]) {
+        [WPNetInterface getProfileWithId:user.profile_id success:^(NSDictionary *profileDic) {
             if (profileDic) {
                 kDefaultSetObjectForKey(profileDic, USER_DEFAULT_PROFILE);
-                _profile = [[WPProfileModel alloc] init];
-                [_profile loadDataFromkeyValues:profileDic];
+                WPProfileModel *profile = [[WPProfileModel alloc] init];
+                [profile loadDataFromkeyValues:profileDic];
                 WPEventModel *event = [[WPEventModel alloc] init];
                 event.status = @"1";
-                event.date = _profile.lastperiod;
+                event.date = profile.lastperiod;
                 event.pid = event.date;
                 [event insertOrupdateToDBDependsOn:nil];
             }else{
-                _profile = nil;
                 kDefaultRemoveForKey(USER_DEFAULT_PROFILE);
             }
             [[NSNotificationCenter defaultCenter] postNotificationName:WPNotificationKeyUpdateProfile object:nil];
@@ -88,16 +81,15 @@
 }
 
 - (void)getDevice{
-    if (![NSString leie_isBlankString:_user.device_id]) {
-        [WPNetInterface getDeviceWithId:_user.device_id success:^(NSDictionary *deviceDic) {
+    WPUserModel *user = [[WPUserModel alloc] init];
+    [user loadDataFromkeyValues:kDefaultObjectForKey(USER_DEFAULT_ACCOUNT_USER)];
+    if (![NSString leie_isBlankString:user.device_id]) {
+        [WPNetInterface getDeviceWithId:user.device_id success:^(NSDictionary *deviceDic) {
             if (deviceDic) {
                 kDefaultSetObjectForKey(deviceDic, USER_DEFAULT_DEVICE);
-                _device = [[WPDeviceModel alloc] init];
-                [_device loadDataFromkeyValues:deviceDic];
                 [[NSNotificationCenter defaultCenter] postNotificationName:WPNotificationKeyUpdateDevice object:nil];
                 [[WPConnectDeviceManager defaultInstance] startTimer];
             }else{
-                _device = nil;
                 kDefaultRemoveForKey(USER_DEFAULT_DEVICE);
             }
         } failure:^(NSError *error) {
@@ -107,50 +99,53 @@
 }
 
 - (void)getTemperatures{
-//    NSString *temp_updatetime = kDefaultObjectForKey(TEMPERATURE_DEFAULT_UPDATETIME);
-    //开始时间当前设备最后一条温度的时间
-    WPTemperatureModel *temperature = [[WPTemperatureModel alloc] init];
-    NSArray *tempsArr = [XJFDBManager searchModelsWithCondition:temperature andpage:0 andOrderby:@"time" isAscend:NO];
-    WPTemperatureModel *localTemp = tempsArr.firstObject;
-    NSString *temp_updatetime = nil;
-    if (localTemp) {
-        temp_updatetime = localTemp.time;
-    }
-    [WPNetInterface getTemperaturesWithUserId:_user.user_id startTime:temp_updatetime end_update_time:nil success:^(NSArray *temperatures) {
-        for (NSDictionary *tempDic in temperatures) {
-            WPTemperatureModel *temp = [[WPTemperatureModel alloc] init];
-            [temp loadDataFromkeyValues:tempDic];
-            [self insertTemperature:temp];
+    WPUserModel *user = [[WPUserModel alloc] init];
+    [user loadDataFromkeyValues:kDefaultObjectForKey(USER_DEFAULT_ACCOUNT_USER)];
+    if (![NSString leie_isBlankString:user.device_id]) {
+        //    NSString *temp_updatetime = kDefaultObjectForKey(TEMPERATURE_DEFAULT_UPDATETIME);
+        //开始时间当前设备最后一条温度的时间
+        WPTemperatureModel *temperature = [[WPTemperatureModel alloc] init];
+        NSArray *tempsArr = [XJFDBManager searchModelsWithCondition:temperature andpage:0 andOrderby:@"time" isAscend:NO];
+        WPTemperatureModel *localTemp = tempsArr.firstObject;
+        NSString *temp_updatetime = nil;
+        if (localTemp) {
+            temp_updatetime = localTemp.time;
         }
-        kDefaultSetObjectForKey([NSNumber numberWithBool:YES], TEMPERATURE_DEFAULT_GETTEMP);
-        [[NSNotificationCenter defaultCenter] postNotificationName:WPNotificationKeyGetTemp object:nil];
-    } failure:^(NSError *error) {
-        kDefaultSetObjectForKey([NSNumber numberWithBool:YES], TEMPERATURE_DEFAULT_GETTEMP);
-        [[NSNotificationCenter defaultCenter] postNotificationName:WPNotificationKeyGetTemp object:nil];
-    }];
+        [WPNetInterface getTemperaturesWithUserId:user.user_id startTime:temp_updatetime end_update_time:nil success:^(NSArray *temperatures) {
+            for (NSDictionary *tempDic in temperatures) {
+                WPTemperatureModel *temp = [[WPTemperatureModel alloc] init];
+                [temp loadDataFromkeyValues:tempDic];
+                [self insertTemperature:temp];
+            }
+            kDefaultSetObjectForKey([NSNumber numberWithBool:YES], TEMPERATURE_DEFAULT_GETTEMP);
+            [[NSNotificationCenter defaultCenter] postNotificationName:WPNotificationKeyGetTemp object:nil];
+        } failure:^(NSError *error) {
+            kDefaultSetObjectForKey([NSNumber numberWithBool:YES], TEMPERATURE_DEFAULT_GETTEMP);
+            [[NSNotificationCenter defaultCenter] postNotificationName:WPNotificationKeyGetTemp object:nil];
+        }];
+    }
 }
 
 - (void)insertTemperature:(WPTemperatureModel *)temp{
-    if (temp.time.length == 9) {
-        NSDate *date = [NSDate dateWithTimeIntervalSince2000:[temp.time longLongValue]];
-        if (date) {
-            WPDeviceModel *device = [[WPDeviceModel alloc] init];
-            [device loadDataFromkeyValues:kDefaultObjectForKey(USER_DEFAULT_DEVICE)];
-            WPTemperatureModel *temperature = [[WPTemperatureModel alloc] init];
-            temperature.date = [NSDate stringFromDate:date format:@"yyyy MM dd"];
-            NSArray *tempsArr = [XJFDBManager searchModelsWithCondition:temperature andpage:-1 andOrderby:@"time" isAscend:NO];
-            WPTemperatureModel *localTemp = tempsArr.firstObject;
-            temp.date = temperature.date;
-            if (localTemp) {
-                if ([temp.time longLongValue] >= [localTemp.time longLongValue]) {
-                    //替换当前记录
-                    [XJFDBManager deleteModel:localTemp dependOnKeys:nil];
-                    [temp insertToDB];
-                }
-            }else{
-                //插入当前时间
+    NSDate *date = [NSDate dateFromString:temp.time format:@"yyyy MM dd HH:mm:ss"];
+    if (date) {
+        WPDeviceModel *device = [[WPDeviceModel alloc] init];
+        [device loadDataFromkeyValues:kDefaultObjectForKey(USER_DEFAULT_DEVICE)];
+        WPTemperatureModel *temperature = [[WPTemperatureModel alloc] init];
+        temperature.date = [NSDate stringFromDate:date format:@"yyyy MM dd"];
+        NSArray *tempsArr = [XJFDBManager searchModelsWithCondition:temperature andpage:-1 andOrderby:@"time" isAscend:NO];
+        WPTemperatureModel *localTemp = tempsArr.firstObject;
+        temp.date = temperature.date;
+        temp.time = [NSString stringWithFormat:@"%f",[date timeIntervalSince2000]] ;
+        if (localTemp) {
+            if ([temp.time longLongValue] >= [localTemp.time longLongValue]) {
+                //替换当前记录
+                [XJFDBManager deleteModel:localTemp dependOnKeys:nil];
                 [temp insertToDB];
             }
+        }else{
+            //插入当前时间
+            [temp insertToDB];
         }
     }
 }
