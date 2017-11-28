6fd7004ffdbdaaf5e5733ff79f3217891213c2a4
yongche <xiaojun@yongche.com>
2017-11-28 09:07:22 +0800

    db version

--- woodpecker/CommonManager/WPDatabaseTableManager.m
+++ woodpecker/CommonManager/WPDatabaseTableManager.m
@@ -15,8 +15,6 @@
 #import "WPPeriodModel.h"
 
 @interface WPDatabaseTableManager ()
-@property(nonatomic, strong) NSString *latestDBVersion;
-@property(nonatomic, strong) NSString *preDBVersion;
 @property(nonatomic, assign) NSInteger latestVersion;
 @property(nonatomic, assign) NSInteger preVersion;
 @end
@@ -27,7 +25,7 @@ Singleton_Implementation(WPDatabaseTableManager);
 - (instancetype)init {
     self = [super init];
     if (self) {
-        _latestDBVersion = @"4";
+        _latestVersion = 1;
     }
     return self;
 }
@@ -46,46 +44,21 @@ Singleton_Implementation(WPDatabaseTableManager);
          */
         [XJFDBManager createTableWithModel:model];
     }
-    //    [[NSUserDefaults standardUserDefaults] setObject:@"2" forKey:USER_DEFAULT_DATABASE_VERSION];
-
-//    self.preDBVersion = [[NSUserDefaults standardUserDefaults] objectForKey:USER_DEFAULT_DATABASE_VERSION];
-//    NSString *userMode = [[NSUserDefaults standardUserDefaults] objectForKey:USER_DEFAULT_USER_MODE];
-//    if (!userMode) {
-//        //新装APP，后续版本可以去掉。因为第一版没有DB version，用userMode来判断是否是新安装。
-//        //新装APP，不需要迁移数据。
-//        [[NSUserDefaults standardUserDefaults] setObject:self.latestDBVersion forKey:USER_DEFAULT_DATABASE_VERSION];
-//    } else if (!self.preDBVersion) {
-//        //从第一版本升级。
-//        [[NSUserDefaults standardUserDefaults] setObject:self.latestDBVersion forKey:USER_DEFAULT_DATABASE_VERSION];
-//        self.preVersion = 1;
-//        self.latestVersion = [self.latestDBVersion integerValue];
-//        [self updateDataBase];
-//    } else if (![self.preDBVersion isEqualToString:self.latestDBVersion]) {
-//        //从非第一版本升级。
-//        [[NSUserDefaults standardUserDefaults] setObject:self.latestDBVersion forKey:USER_DEFAULT_DATABASE_VERSION];
-//        self.preVersion = [self.preDBVersion integerValue];
-//        self.latestVersion = [self.latestDBVersion integerValue];
-//        [self updateDataBase];
-//    }
+    
+    if (![[NSUserDefaults standardUserDefaults] objectForKey:USER_DEFAULT_DATABASE_VERSION]) {
+        //新装app不用升级
+        [[NSUserDefaults standardUserDefaults] setObject:[NSNumber numberWithInteger:_latestVersion] forKey:USER_DEFAULT_DATABASE_VERSION];
+    } else if (_latestVersion != [[[NSUserDefaults standardUserDefaults] objectForKey:USER_DEFAULT_DATABASE_VERSION] integerValue]) {
+        //从非第一版本升级。
+        self.preVersion = [[[NSUserDefaults standardUserDefaults] objectForKey:USER_DEFAULT_DATABASE_VERSION] integerValue];
+        [[NSUserDefaults standardUserDefaults] setObject:[NSNumber numberWithInteger:_latestVersion] forKey:USER_DEFAULT_DATABASE_VERSION];
+        [self updateDataBase];
+    }
 }
 
 - (void)updateDataBase {
 //    switch (self.preVersion) {
 //        case 1: {
-//            NSString *updateSQL = @"alter table UNICCommonOrderModel add is_temperature_uploaded INTEGER default 1";
-//            [[XJFDBOperator defaultInstance] executeSQLs:updateSQL];
-//
-//            NSString *updateSQL2 = @"alter table UNICCommonTemperatureModel add order_pid TEXT";
-//            [[XJFDBOperator defaultInstance] executeSQLs:updateSQL2];
-//
-//            UNICCommonOrderModel *orderCondition = [[UNICCommonOrderModel alloc] init];
-//            orderCondition.order_type = 2;
-//            NSArray<UNICCommonOrderModel *> *data = [XJFDBManager searchModelsWithCondition:orderCondition andpage:-1 andOrderby:nil isAscend:NO];
-//            for (UNICCommonOrderModel *order in data) {
-//                NSString *updateSQL3 =
-//                    [NSString stringWithFormat:@"update UNICCommonTemperatureModel set order_pid = '%@' where order_id = %ld", order.pid, (long)order.order_id];
-//                [[UNICDBOperator defaultInstance] executeSQLs:updateSQL3];
-//            }
 //        } break;
 //        default:
 //            break;
